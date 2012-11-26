10a31a70df2ad952bb160a11b2a177442ebffab5
Mathyomama <Mathyomama@gmail.com>
2012-11-26 02:23:08 -0500

    stuff

--- euler66.py
+++ euler66.py
@@ -1,16 +1,32 @@
-maxTuple = (0, 0)
-tuples = []
-for D in range(1, 1001):
-	if D**.5%1 == 0: continue
-	y, x = 1, 0
-	while True:
-		x = (D*y**2 + 1)**.5
-		if x%1 == 0:
-			break
-		y += 1
-	tuples.append((D, x))
-	print((D, x, y))
-	#if tuples[-1][1] > maxTuple[1]:
-	#	maxTuple = (D, x)
-	#	print(maxTuple)
+from math import *
 
+def infinite_fraction(D):
+	alpha = D**.5
+	a = floor(alpha)
+	DList, DTuple = [a], ()
+	for i in range(100):
+		alpha = 1/(alpha - a)
+		a = floor(alpha)
+		DTuple += (a,)
+		print(a, alpha)
+	DList += DTuple
+	return DList
+
+def comp(x, y, z):
+	return (z, x*z + y)
+
+def composition(n, DList):
+	tries = n - 1
+	period = len(DList[1])
+	arg = DList[1][tries%period]
+	denum = (1, arg)
+	while tries > 0:
+		tries -= 1
+		arg = DList[1][tries%period]
+		denum = comp(arg, *denum)
+	denum = (DList[0]*denum[1] + denum[0], denum[1])
+	return denum
+
+thirteenList = [3, (1, 1, 1, 1, 6)]
+for i in range(10):
+	print(composition(i, thirteenList))
