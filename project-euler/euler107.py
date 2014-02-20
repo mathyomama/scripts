67c61fb0b36fd7b7388509bfdcd0d35d52c615dc
mathyomama <mathyomama@gmail.com>
2014-02-20 17:19:20 -0500

    more euler107

--- project-euler/euler107.py
+++ project-euler/euler107.py
@@ -6,7 +6,7 @@ import sys
 
 def modifiedInt(str):
 	if str == "-" or str == "-\n":
-		return "-"
+		return 0
 	else:
 		return int(str)
 
@@ -14,26 +14,38 @@ def findMinimalConnectedSum(network):
 	length = len(network)
 	touchedNetwork = [[0]*length]*length
 	touchedVertices = [0]*length
+	touchedVertices[0] = 1
 	touchCount = sum(touchedVertices)
 	minSum = 0
 	while touchCount < length:
-		minPath, minIndex = 1000, length
-		if totalSum == 0:
-			for index, val in enumerate(network[0]):
-				if val < minPath:
-					minPath = val
-					minIndex = index
-			minSum += minPath
-		else:
-			pass
+		minVal, minIndex, minRow = 1000, length, length
+		badVal, badIndex = 1000, length
+		for row, switch in enumerate(touchedVertices):
+			if switch == 1:
+				for index, val in enumerate(network[row]):
+					if val != 0 and touchedVertices[index] == 0 and val < minVal:
+						minVal, minIndex, minRow = val, index, row
+		touchedVertices[minIndex] = 1
+		touchedNetwork[minRow][minIndex], touchedNetwork[minIndex][minRow] = minVal, minVal
+		touchCount = sum(touchedVertices)
+	return touchedNetwork
 
 def main():
 	foo = open("network.txt")
 	network = [map(modifiedInt, line.split(",")) for line in foo]
-	for row in network:
-		for element in row:
-			print element, "\t",
-		print ""
+	foo.close()
+	originalSum = sum(map(sum, network))
+	touchedNetwork = findMinimalConnectedSum(network)
+	networkFile = open("networkFile.csv", "w")
+	touchedNetworkFile = open("touchedNetwork.csv", "w")
+	for row, touchedRow in zip(network, touchedNetwork):
+		for element, touchedElement in zip(row, touchedRow):
+			networkFile.write(str(element) + ";")
+			touchedNetworkFile.write(str(touchedElement) + ";")
+		networkFile.write("\n")
+		touchedNetworkFile.write("\n")
+	networkFile.close()
+	touchedNetworkFile.close()
 
 if __name__ == "__main__":
 	sys.exit(main())
